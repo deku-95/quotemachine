5fdfebb05b5a999b50864c8b7ffdfc83e2612d18
Alejandro Gil Casco <alexgilcasco@gmail.com>
2021-11-30 18:51:12 +0900

    Logic is working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import * as React from "react";
+
+const url =
+  "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          quote: data.quotes[Math.floor(Math.random() * data.quotes.length)],
+        });
+      });
+  }
+
   render() {
     return (
-      <div id="App" className="pure-g">
-        <div className="pure-u-1-3">First</div>
-        <div className="pure-u-1-3">Second</div>
-        <div className="pure-u-1-3">Third</div>
+      <div className="App">
+        <h1>{this.state.quote.quote}</h1>
+        <h2>{this.state.quote.author}</h2>
+        <button onClick={() => this.componentDidMount()}>New Quote</button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "purecss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>,
   document.getElementById("root")
 );
