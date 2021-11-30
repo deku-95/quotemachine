faddf228dca4a982f7f4d954eaf65bc2c2f28902
Alejandro Gil Casco <alexgilcasco@gmail.com>
2021-12-01 03:29:10 +0900

    Added FCC Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote: "",
+      quote: "Loading...",
     };
   }
 
@@ -19,24 +19,47 @@ class App extends React.Component {
           quote: data.quotes[Math.floor(Math.random() * data.quotes.length)],
         });
       });
+
+    const script = document.createElement("script");
+    script.src =
+      "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
+    script.async = true;
+    document.body.appendChild(script);
   }
 
   render() {
+    var tweet =
+      "https://twitter.com/intent/tweet?text=" +
+      '"' +
+      this.state.quote.quote +
+      '" -' +
+      this.state.quote.author;
+
     return (
       <div className="App">
-        <div class="pure-g center">
-          <div class="pure-u-1-1">
-            <h1 class="center">Random Quote Machine</h1>
-            <div id="quote">
-              <p id="quoteText">"{this.state.quote.quote}"</p>
+        <div className="pure-g center">
+          <div className="pure-u-1-1">
+            <h1 className="center">Random Quote Machine</h1>
+            <div id="quote-box">
+              <p id="text">"{this.state.quote.quote}"</p>
               <p id="author">-{this.state.quote.author}</p>
             </div>
             <button
-              class="pure-button"
+              id="new-quote"
+              className="pure-button"
               onClick={() => this.componentDidMount()}
             >
               New Quote
             </button>
+            <a
+              id="tweet-quote"
+              className="pure-button"
+              href={tweet}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Tweet this quote
+            </a>
           </div>
         </div>
       </div>
